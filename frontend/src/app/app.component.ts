ab90aa852e386bc71264ea912333ac54769cd452
Bogdan Liusik <liusikbogdan@gmail.com>
2025-08-02 18:12:07 +0300

    clear frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
-import { Dialog } from 'primeng/dialog';
-import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, Dialog, InputTextModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'eternity';
 
-  visible: boolean = false;
-
-  showDialog() {
-    this.visible = true;
-  }
-
-  public toggleDarkMode() {
-    const element = document.querySelector('html');
-    element?.classList.toggle('eternity-dark');
-  }
 }
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,15 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import EternityTheme from '@/themes/eternity-theme';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideAnimationsAsync(),
-    providePrimeNG({ theme: EternityTheme, ripple: false })
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
