eba9fe91f3f4ca1d87c43e8d0352f3901e5ee2e7
Bogdan Liusik <liusikbogdan@gmail.com>
2025-09-01 19:47:09 +0300

    feature(layout): sidebar menu overlay event handling

--- frontend/src/app/features/layout/components/sidebar/sidebar-menu/sidebar-menu.ts
+++ frontend/src/app/features/layout/components/sidebar/sidebar-menu/sidebar-menu.ts
@@ -1,11 +1,11 @@
-import { Component, HostListener, inject } from '@angular/core';
+import { Component, inject, QueryList, ViewChildren } from '@angular/core';
 import { SidebarSubmenu } from '../sidebar-submenu/sidebar-submenu';
 import { Plus, Minus, LucideAngularModule } from 'lucide-angular';
 import { MenuService } from '../../../services/menu.service';
 import { SubMenuItem } from '../../../types/sub-menu-item';
 import { NgClass } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { ConnectedPosition, Overlay, OverlayModule } from '@angular/cdk/overlay';
+import { CdkOverlayOrigin, ConnectedPosition, Overlay, OverlayModule } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-sidebar-menu',
@@ -14,9 +14,14 @@ import { ConnectedPosition, Overlay, OverlayModule } from '@angular/cdk/overlay'
   styleUrl: './sidebar-menu.css'
 })
 export class SidebarMenu {
+  @ViewChildren(CdkOverlayOrigin, { read: CdkOverlayOrigin })
+  private triggers!: QueryList<CdkOverlayOrigin>;
+  private overlay = inject(Overlay);
+
   public menuService = inject(MenuService);
   public plusIcon = Plus;
   public minusIcon = Minus;
+  public repositionsStrategy = this.overlay.scrollStrategies.reposition();
 
   public toggleMenu(subMenu: SubMenuItem) {
     if (!this.menuService.showSideBar) {
@@ -26,15 +31,22 @@ export class SidebarMenu {
     }
   }
 
-  private overlay = inject(Overlay);
-
   overlayPositions = [
     { originX: 'end', originY: 'top', overlayX: 'start', overlayY: 'top', offsetX: 8 },
     { originX: 'end', originY: 'bottom', overlayX: 'start', overlayY: 'bottom', offsetX: 8 },
     { originX: 'start', originY: 'top', overlayX: 'end', overlayY: 'top', offsetX: -8 }
   ] as const satisfies ConnectedPosition[];
 
-  scrollStrategies = {
-    reposition: this.overlay.scrollStrategies.reposition()
-  };
+  onOverlayOutsideClick(event: MouseEvent) {
+    const target = event.target as Node | null;
+    if (!target) {
+      this.menuService.closeAllDropdowns();
+      return;
+    }
+    const clickedOnAnyTrigger = this.triggers?.toArray().some((t) => t.elementRef.nativeElement.contains(target));
+    if (clickedOnAnyTrigger) {
+      return;
+    }
+    this.menuService.closeAllDropdowns();
+  }
 }
